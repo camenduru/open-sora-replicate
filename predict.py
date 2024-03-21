7c4ef8ba86562412693f3fe467f2526280de9086
camenduru <54370274+camenduru@users.noreply.github.com>
2024-03-21 15:53:09 +0300

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,18 @@
+import os
+from cog import BasePredictor, Input, Path
+from pyngrok import ngrok, conf
+
+class Predictor(BasePredictor):
+    def setup(self) -> None:
+        directory = "/content"
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+    def predict(
+        self,
+        token: str = Input()
+    ) -> str:
+        conf.get_default().auth_token = token
+        public_url = ngrok.connect(7860).public_url
+        print(public_url)
+        os.system(f"jupyter notebook --allow-root --port 7860 --ip 0.0.0.0 --NotebookApp.token '' --no-browse --notebook-dir /content")
+        return public_url
